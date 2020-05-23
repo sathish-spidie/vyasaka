c22c8e25466c8b1bcda93d0515c4b0c4972376a7
sathish-spidie <sathishshaj9484@gmail.com>
2020-05-23 16:56:02 +0530

    Intial Commit

--- src/types.js
+++ src/types.js
@@ -1,5 +1,3 @@
 export const USER_LOGGED_IN = "USER_LOGGED_IN";
 export const USER_LOGGED_OUT = "USER_LOGGED_OUT";
 export const USER_REGISTER = "USER_REGISTER";
-export const BOOK_FETCHED = "BOOK_FETCHED";
-export const BOOK_CREATED = "BOOK_CREATED";
